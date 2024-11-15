e98799d61bb5ba851d3801db1ad8da5ef9862c6f
ihab abadi <ihab@utopios.net>
2024-11-15 12:02:54 +0100

    avec senario 3

--- solution-tp-final/command_scenario3.sh
+++ solution-tp-final/command_scenario3.sh
@@ -2,3 +2,11 @@ sudo chmod +x /usr/local/bin/cleanup_backups.sh
 sudo crontab -e
 0 1 * * * /usr/local/bin/backup_gitea.sh
 0 2 * * 0 /usr/local/bin/cleanup_backups.sh
+
+sudo systemctl enable backup_gitea.timer
+sudo systemctl start backup_gitea.timer
+
+sudo systemctl enable cleanup_backups.timer
+sudo systemctl start cleanup_backups.timer
+
+systemctl list-timers
